c716279f4eaee97aafafc2a7b438d8f186f64057
gaetanoCultrera <gaetanozhhz@gmail.com>
2023-09-11 19:26:09 +0200

    feat/first version maps

--- screens/theater/TheaterScreen.tsx
+++ screens/theater/TheaterScreen.tsx
@@ -1,6 +1,43 @@
-import React from "react";
-import { Text } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export const TheaterScreen = () => {
-  return <Text>Cinema</Text>;
+  const [location, setLocation] = useState<Location.LocationObject>();
+
+  useEffect(() => {
+    void (async () => {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+      }
+    })();
+  }, []);
+  //funziona con un dispositivo alla volta, utilizza expo go
+  return (
+    <View style={{ flex: 1 }}>
+      {location && (
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="La tua posizione"
+          />
+        </MapView>
+      )}
+    </View>
+  );
 };
