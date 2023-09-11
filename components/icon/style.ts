126a6a24564ac32ca125fe2c1c64facf8afac0d2
gaetanoCultrera <gaetanozhhz@gmail.com>
2023-09-11 16:37:13 +0200

    fix/close camera when screen change

--- components/icon/style.ts
+++ components/icon/style.ts
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 export const { containerBadge } = StyleSheet.create({
   containerBadge: {
     position: "absolute",
-    right: 15,
+    bottom: 0,
+    right: 20,
   },
 });
