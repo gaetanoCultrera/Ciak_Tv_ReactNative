db69c75d33b9796a9b4ac708a9b1515137c3cb48
gaetanoCultrera <gaetanozhhz@gmail.com>
2023-09-13 16:50:48 +0200

    fix

--- components/card/profileCard/ProfileCard.tsx
+++ components/card/profileCard/ProfileCard.tsx
@@ -5,7 +5,6 @@ import { container } from "../style";
 import { IPropsProfileCamera } from "../../../interfaces/IPropsProfileCamera";
 import { CardHeader } from "./utils/cardHeader/CardHeader";
 
-<<<<<<< HEAD
 export const ProfileCard: FC<IPropsProfileCamera> = memo(
   ({ setShowCamera }) => {
     return (
@@ -16,38 +15,3 @@ export const ProfileCard: FC<IPropsProfileCamera> = memo(
     );
   }
 );
-=======
-const ProfileCard = () => {
-  const { username, email } = useSelector(
-    ({ objectSignUp }: RootState) => objectSignUp.dataSignup
-  );
-
-  const handleSetLogout = useHandleSetLogout();
-  const handleDeleteAccount = useHandleSetDeleteAccount();
-
-  const leftContent = useCallback(
-    () => <AvatarIcon nameIcon={"account"} />,
-    []
-  );
-
-  return (
-    <Card style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Card.Title
-        title={<Text variant="titleLarge">Hello! {username}</Text>}
-        subtitle={<Text variant="bodyMedium">{email}</Text>}
-        left={leftContent}
-      />
-      <Card.Actions>
-        <Button icon={"account-off"} onPress={handleSetLogout}>
-          Logout
-        </Button>
-        <Button icon={"delete"} onPress={handleDeleteAccount}>
-          Delete Account
-        </Button>
-      </Card.Actions>
-    </Card>
-  );
-};
-
-export default ProfileCard;
->>>>>>> origin/feat/biometric-auth
