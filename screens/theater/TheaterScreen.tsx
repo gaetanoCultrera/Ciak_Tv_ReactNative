3e7c04575881317f09c8a12134fe388f04f59813
gaetanoCultrera <gaetanozhhz@gmail.com>
2023-09-13 17:41:17 +0200

    refactor/Theather screen

--- screens/theater/TheaterScreen.tsx
+++ screens/theater/TheaterScreen.tsx
@@ -1,38 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import * as Location from "expo-location";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObjectCoords,
+} from "expo-location";
 
 export const TheaterScreen = () => {
-  const [location, setLocation] = useState<Location.LocationObject>();
+  const [coords, setCoords] = useState<LocationObjectCoords>();
 
   useEffect(() => {
     void (async () => {
-      const { granted } = await Location.requestForegroundPermissionsAsync();
+      const { granted } = await requestForegroundPermissionsAsync();
 
       if (granted) {
-        const location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
+        const { coords } = await getCurrentPositionAsync({});
+        setCoords(coords);
       }
     })();
   }, []);
   //funziona con un dispositivo alla volta, utilizza expo go
   return (
     <View style={{ flex: 1 }}>
-      {location && (
+      {coords && (
         <MapView
           style={{ flex: 1 }}
           initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
+            latitude: coords.latitude,
+            longitude: coords.longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
         >
           <Marker
             coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
+              latitude: coords.latitude,
+              longitude: coords.longitude,
             }}
             title="La tua posizione"
           />
