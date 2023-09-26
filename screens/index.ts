565cd049676dbc67d3502251cbdce40b522fa27f
gaetanoCultrera <gaetanozhhz@gmail.com>
2023-09-26 10:53:39 +0200

    fix:index file in screens folder

--- screens/index.ts
+++ screens/index.ts
@@ -1,5 +1,17 @@
 import { DashboardScreen } from "./dashboard/DashboardScreen";
 import { ProfileScreen } from "./profile/ProfileScreen";
 import { AuthScreen } from "./auth/AuthScreen";
+import { DetailsScreen } from "./details/DetailsScreen";
+import { FavoriteScreen } from "./favorite/FavoriteScreen";
+import { SearchScreen } from "./search/SearchScreen";
+import { TheaterScreen } from "./theater/TheaterScreen";
 
-export { DashboardScreen, ProfileScreen, AuthScreen };
+export {
+  DashboardScreen,
+  ProfileScreen,
+  AuthScreen,
+  DetailsScreen,
+  FavoriteScreen,
+  SearchScreen,
+  TheaterScreen,
+};
